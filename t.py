93a2a0244e6f3e495b6aff505f1a3952bf3e159f
zhangzh666123 <79248380+zhangzh666123@users.noreply.github.com>
2023-04-18 22:35:21 +0800

    Delete t.py

--- t.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from PyQt6 import QtWidgets
-import sys
-app = QtWidgets.QApplication(sys.argv)
-test_window = QtWidgets.QWidget()
-test_window.resize(500,500)
-test_window.setWindowTitle("测试窗口")
-test_window.show()
-sys.exit(app.exec())
